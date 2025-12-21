f7d7f16a8ac73942dfacfd2f5edce9b8cdf0fa11
inblossoms <L13994352192@163.com>
2025-12-21 18:02:21 +0800

    chore: Stubbed err handling

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,26 @@ mod parser;
 mod reader;
 mod tokenizer;
 
+type Error = ();
+
 fn main() {
     println!("Hello, lox!");
 
+    match run() {
+        Ok(out) => {
+            println!("Success! {:?}", out);
+        }
+        Err(e) => {
+            println!("Work goes wrong, failed info: {:?}", e);
+        }
+    }
+}
+
+fn run() -> Result<(), Error> {
     let source = reader::reader_source("file.lox").unwrap();
     let tokens = tokenizer::tokenize(source).unwrap();
     let ast = parser::parse(tokens).unwrap();
     let out = evaluate::evaluate(ast).unwrap();
+
+    Ok(())
 }
